3fb3be6672e6f58f4e097fdfb02b195a91caba60
DESKTOP-D7NMFO9\Sandeep <www.sandeepbabu45gmail.com>
2019-10-15 11:57:06 +0530

    Log on Home

--- app/src/main/java/com/sandeep/mindgame/memo/HomeActivity.kt
+++ app/src/main/java/com/sandeep/mindgame/memo/HomeActivity.kt
@@ -2,6 +2,7 @@ package com.sandeep.mindgame.memo
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.util.Log
 import com.sandeep.mindgame.R
 
 class HomeActivity : AppCompatActivity() {
@@ -9,5 +10,7 @@ class HomeActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_home)
+
+        Log.d("Home","this is home")
     }
 }
